90d9444a048fbc12fc407402375a0af221e20a08
HamdiHarahap <hamdiharahap2005@gmail.com>
2025-07-27 23:06:16 +0700

    Refactor NoteList component to manage notes state and implement delete and archive functionality

--- src/features/notes/components/NoteList/index.jsx
+++ src/features/notes/components/NoteList/index.jsx
@@ -1,7 +1,55 @@
-const NoteList = () => {
+import { useEffect, useState } from 'react';
+import NoteCard from '../NoteCard';
+
+const NoteList = (props) => {
+	const { isArchived, status } = props;
+	const [notes, setNotes] = useState([]);
+	const [refreshTrigger, setRefreshTrigger] = useState(false);
+
+	useEffect(() => {
+		const allNotes = JSON.parse(localStorage.getItem('notes')) || [];
+		const filtered = isArchived
+			? allNotes.filter((note) => note.is_archived === true)
+			: allNotes.filter((note) => note.is_archived === false);
+		setNotes(filtered);
+	}, [isArchived, refreshTrigger]);
+
+	const handleDelete = (id) => {
+		const allNotes = JSON.parse(localStorage.getItem('notes')) || [];
+		const updatedNotes = allNotes.filter((note) => note.id !== id);
+		localStorage.setItem('notes', JSON.stringify(updatedNotes));
+		setRefreshTrigger((prev) => !prev);
+	};
+
+	const handleArchived = (id) => {
+		const allNotes = JSON.parse(localStorage.getItem('notes')) || [];
+		const updatedStatus = allNotes.map((note) => {
+			if (note.id === id) {
+				return {
+					...note,
+					is_archived: !note.is_archived,
+				};
+			}
+			return note;
+		});
+
+		localStorage.setItem('notes', JSON.stringify(updatedStatus));
+		setRefreshTrigger((prev) => !prev);
+	};
+
 	return (
-		<div>
-			<div></div>
+		<div className="grid grid-cols-3 gap-8 max-[900px]:grid-cols-2">
+			{notes.map((note) => (
+				<NoteCard
+					status={status}
+					handleDelete={() => handleDelete(note.id)}
+					handleArchived={() => handleArchived(note.id)}
+					key={note.id}
+					title={note.title}
+					body={note.body}
+					createdAt={note.created_at}
+				/>
+			))}
 		</div>
 	);
 };
