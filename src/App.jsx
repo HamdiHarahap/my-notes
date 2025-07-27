80093c3337fd85eb920280d706e7ed35fc5a3245
HamdiHarahap <hamdiharahap2005@gmail.com>
2025-07-27 23:05:51 +0700

    Add note status management and update UI for active/archived notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import Button from './components/ui/Button';
 import NoteForm from './features/notes/components/NoteForm';
+import NoteList from './features/notes/components/NoteList';
 
 const App = () => {
 	const [showForm, setShowForm] = useState(false);
+	const [openStatus, setOpenStatus] = useState('active');
 
 	const handleShowForm = () => {
 		setShowForm(!showForm);
@@ -11,6 +13,12 @@ const App = () => {
 
 	const handleCloseForm = () => {
 		setShowForm(false);
+	};
+
+	const handleOpenStatus = () => {
+		const newStatus = openStatus === 'active' ? 'archived' : 'active';
+		setOpenStatus(newStatus);
+		localStorage.setItem('status', JSON.stringify(newStatus));
 	};
 
 	return (
@@ -34,12 +42,45 @@ const App = () => {
 				</nav>
 				<div className="px-44 py-8">
 					<Button
-						className="px-5 py-2 rounded-md bg-zinc-900 text-white cursor-pointer font-semibold"
+						className="px-5 py-2 rounded-md bg-zinc-900 text-white cursor-pointer font-semibold hover:bg-zinc-800 transition"
 						onClick={handleShowForm}
 					>
 						Create Note
 					</Button>
 				</div>
+			</div>
+
+			<div className="flex gap-6 px-44 mb-10">
+				<Button
+					disabled={openStatus === 'active'}
+					className={`font-semibold rounded-md px-8 py-2 cursor-pointer ${
+						openStatus == 'active'
+							? 'bg-zinc-900 text-white'
+							: 'bg-none text-black'
+					}`}
+					onClick={handleOpenStatus}
+				>
+					Active Notes
+				</Button>
+				<Button
+					disabled={openStatus === 'archived'}
+					className={`font-semibold rounded-md px-8 py-2 cursor-pointer ${
+						openStatus == 'archived'
+							? 'bg-zinc-900 text-white'
+							: 'bg-none text-black'
+					}`}
+					onClick={handleOpenStatus}
+				>
+					Archive Notes
+				</Button>
+			</div>
+
+			<div className="px-44">
+				{openStatus === 'active' ? (
+					<NoteList isArchived={false} status={openStatus} />
+				) : (
+					<NoteList isArchived={true} status={openStatus} />
+				)}
 			</div>
 		</>
 	);
